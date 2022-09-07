c1438fd9e4d86b020b09bcc37157e94c643acf44
Lauren <lel15@icloud.com>
2022-09-07 16:46:29 -0700

    Put file contents into a SHA1 successfully

--- src/Blob.java
+++ src/Blob.java
@@ -10,13 +10,13 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+import java.util.Arrays;
 
 public class Blob {
-	private byte[] byteArray;
-	private String SHA1;
+	private byte[] SHA1;
+	private String SHA1Str;
 	
-	public Blob(String path) throws IOException
+	public Blob(String path) throws IOException, NoSuchAlgorithmException
 	{
 		FileInputStream fstream = new FileInputStream("blobTestFile.txt");
 		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
@@ -33,31 +33,43 @@ public class Blob {
 
 		//Close the input stream
 		fstream.close();
-//		SHA1 = toSHA1(byteArray);
+		SHA1(str);
 //		newFile();
 	}
 	
-	public static String toSHA1(byte[] convertme) {
-	    MessageDigest md = null;
-	    try {
-	        md = MessageDigest.getInstance("SHA-1");
-	    }
-	    catch(NoSuchAlgorithmException e) {
-	        e.printStackTrace();
-	    } 
-	    return new String(md.digest(convertme));
+	public void SHA1(String x) throws NoSuchAlgorithmException
+	{
+
+	    MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+	    SHA1 = sha1.digest((x).getBytes()); 
+	    for(byte b : SHA1 ) {
+	    	  System.out.printf("%02x",b);
+	    	}
+	    	System.out.println();
+
 	}
 	
-	public void newFile() throws FileNotFoundException
-	{
-		System.out.println ("./Blob/" + SHA1);
-		File SHA1File = new File("./objects/" + SHA1);
-		//String destination = ("./objects/" + SHA1);
-		PrintWriter out = new PrintWriter("SHA1File");
-		String str1 = new String(byteArray);
-		out.print(str1);
-		out.close();
-	}
+//	public static String toSHA1(byte[] convertme) {
+//	    MessageDigest md = null;
+//	    try {
+//	        md = MessageDigest.getInstance("SHA-1");
+//	    }
+//	    catch(NoSuchAlgorithmException e) {
+//	        e.printStackTrace();
+//	    } 
+//	    return new String(md.digest(convertme));
+
+	
+//	public void newFile() throws FileNotFoundException
+//	{
+////		System.out.println ("./Blob/" + SHA1);
+//		File SHA1File = new File("./objects/" + SHA1);
+//		//String destination = ("./objects/" + SHA1);
+//		PrintWriter out = new PrintWriter("SHA1File");
+//		String str1 = new String(byteArray);
+//		out.print(str1);
+//		out.close();
+//	}
 	
 	
 	
--- src/BlobTester.java
+++ src/BlobTester.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
 public class BlobTester {
-	public static void main (String[] args) throws IOException 
+	public static void main (String[] args) throws IOException, NoSuchAlgorithmException 
 	{
 		Blob blob1 = new Blob("Blob/blobTestFile");	
 	}
